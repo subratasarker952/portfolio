738c4c1e38c94396bc8666eeeaab1f44381cf9da
subratasarker952 <subratasarker952@gmail.com>
2025-05-29 21:11:34 +0600

    add get resume button

--- src/HomePageSection/Hero.jsx
+++ src/HomePageSection/Hero.jsx
@@ -13,12 +13,21 @@ const Hero = () => {
         <p className="text-xl text-gray-600 mb-4">
           MERN Stack Developer & Medical Technologist
         </p>
-        <a
-          href="#contact"
-          className="inline-block bg-indigo-600 text-white px-5 py-2 rounded-lg font-semibold shadow hover:bg-indigo-700 transition"
-        >
-          Contact Me
-        </a>
+        <div className='flex gap-2'>
+          <a
+            href="#contact"
+            className="inline-block whitespace-nowrap bg-indigo-600 text-white px-5 py-2 rounded-lg font-semibold shadow hover:bg-indigo-700 transition"
+          >
+            Contact Me
+          </a>
+          <a
+            href="https://drive.google.com/file/d/1_EpWP2313rOU-Er3yTodThs37oX3FCiY/view"
+            target='_blank'
+            className="inline-block whitespace-nowrap bg-indigo-600 text-white px-5 py-2 rounded-lg font-semibold shadow hover:bg-indigo-700 transition"
+          >
+            Get Resume
+          </a>
+        </div>
       </div>
     </section>
   );
